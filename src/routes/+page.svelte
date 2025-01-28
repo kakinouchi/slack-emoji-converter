274d75b7a6f0447babbff1554e0fd27e2a004188
kakinouchi <32264080+kakinouchi@users.noreply.github.com>
2025-01-28 23:24:41 +0900

    Impl state logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,23 @@
-<!-- <h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+<script lang="ts">
+	let messageByClipboard: string = $state('');
+	let messageInInputArea: string = $state('');
+	let messageInOutputArea: string = $derived(messageInInputArea);
+
+	// function handleButtonClick(event: { currentTarget: HTMLButtonElement }) {
+	// 	console.log('foo');
+	// }
+	function handleTextareaPaste(event: ClipboardEvent) {
+		messageByClipboard = event?.clipboardData?.getData('text') || '';
+
+		if (!messageInInputArea) {
+			messageInInputArea = messageByClipboard;
+		}
+	}
+
+	function handleTextareaChange(event: { currentTarget: HTMLTextAreaElement }) {
+		messageInInputArea = event?.currentTarget?.value;
+	}
+</script>
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="" />
@@ -10,7 +27,7 @@
 	/>
 	<title>Slack Emoji Converter</title>
 	<link rel="icon" type="image/x-icon" href="data:image/x-icon;base64," />
-	<script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script>
+	<!-- <script src="https://cdn.tailwindcss.com?plugins=forms,container-queries"></script> -->
 </svelte:head>
 
 <div
@@ -32,6 +49,8 @@
 			<label class="flex min-w-40 flex-1 flex-col">
 				<p class="pb-2 text-base font-medium leading-normal text-[#111518]">Input</p>
 				<textarea
+					onpaste={handleTextareaPaste}
+					onchange={handleTextareaChange}
 					placeholder="Type your message here"
 					class="form-input flex min-h-96 w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl border border-[#dbe1e6] bg-white p-[15px] text-base font-normal leading-normal text-[#111518] placeholder:text-[#60768a] focus:border-[#dbe1e6] focus:outline-0 focus:ring-0"
 				></textarea>
@@ -44,15 +63,17 @@
 				<textarea
 					placeholder="Converted emojis will appear here"
 					class="form-input flex min-h-96 w-full min-w-0 flex-1 resize-none overflow-hidden rounded-xl border border-[#dbe1e6] bg-white p-[15px] text-base font-normal leading-normal text-[#111518] placeholder:text-[#60768a] focus:border-[#dbe1e6] focus:outline-0 focus:ring-0"
-				></textarea>
+					>{messageInOutputArea}</textarea
+				>
 			</label>
 		</div>
 	</div>
 
 	<div>
-		<div class="flex justify-center">
+		<!-- <div class="flex justify-center">
 			<div class="flex max-w-[480px] flex-1 flex-col items-stretch gap-3 px-4 py-3">
 				<button
+					onclick={handleButtonClick}
 					class="flex h-10 w-full min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl bg-[#2590f4] px-4 text-sm font-bold leading-normal tracking-[0.015em] text-white"
 				>
 					<span class="truncate">Convert</span>
@@ -63,7 +84,7 @@
 					<span class="truncate">Clear</span>
 				</button>
 			</div>
-		</div>
+		</div> -->
 		<footer class="flex flex-col gap-6 px-5 py-10 text-center @container">
 			<p class="text-base font-normal leading-normal text-[#60768a]">
 				<!-- 2023 Emoji Inc -->
